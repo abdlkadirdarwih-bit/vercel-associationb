e4a2d4580558bb0a0f63887f59da3401eaa134a2
abdlkader <abdlkadirdarwih@gmail.com>
2026-02-08 20:27:55 +0200

    Update backend for Vercel  deployment

--- index.js
+++ index.js
@@ -65,19 +65,11 @@ const allowedOrigins = [
   process.env.FRONTEND_URL_LOCAL,
   process.env.FRONTEND_URL_PROD
 ];
+
 app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin (Postman, curl)
-    if(!origin) return callback(null, true);
-
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = `CORS policy: ${origin} not allowed`;
-      return callback(new Error(msg), false);
-    }
-
-    return callback(null, true);
-  },
-  credentials: true
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 // app.use(cors());
 
